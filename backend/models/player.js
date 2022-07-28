bc87db5d8da7e0f4527165d4f3f4d34a1a20d57c
Anushkagupta1612 <anushkagupta2424@gmail.com>
2022-07-28 14:52:35 +0530

    AlmostAlmostFinal

--- backend/models/player.js
+++ backend/models/player.js
@@ -1,23 +1,28 @@
 // player score 
 // array of user addresses
 
-const mongoose = require('mongoose')
+const mongoose = require( 'mongoose' )
 
 const player = mongoose.Schema(
-    {
-        playerId: { type: Number},
-        score: {type: Number},  
-        history : [
-          {
-            type: String,
-          },
-        ]
-    },
-    {
-      timestamps: {type: Date,default: Date.now()},
-    }
+  {
+    playerId: { type: Number },
+    score: { type: Number },
+    history: [
+      {
+        type: String,
+      },
+    ],
+    tokenIds: [
+      {
+        type: Number,
+      },
+    ]
+  },
+  {
+    timestamps: { type: Date, default: Date.now() },
+  }
 );
 
-const Player = mongoose.model('Player',player)
+const Player = mongoose.model( 'Player', player )
 
 module.exports = Player;
--- backend/routes/profile.js
+++ backend/routes/profile.js
@@ -1,50 +1,64 @@
-const express = require('express')
+const express = require( 'express' )
 const router = express.Router()
-const User = require('../models/User')
-const Auction = require('../models/Auction')
-const Player = require('../models/Player')
+const User = require( '../models/User' )
+const Auction = require( '../models/Auction' )
+const Player = require( '../models/Player' )
 
 // As he clicks the sell button 
 // the user a PATCH request to the User table to change 
 // the sell state to True and set the sell Amount
-router.patch('/:id', async function (req, res) {
+router.patch( '/:id', async function ( req, res ) {
     try {
         const _id = req.params.id
-        const updatedData = await User.findOneAndUpdate({address:_id},req.body,{ new: true})
-        res.send(updatedData)
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json("Some error occured!")      
+        const updatedData = await User.findOneAndUpdate( { address: _id }, req.body, { new: true } )
+        res.send( updatedData )
+    } catch ( error ) {
+        console.log( error.message )
+        res.status( 500 ).json( "Some error occured!" )
     }
-})
+} )
 
 // GET request to User Table to get 
 // Sell Amount, Position in Leaderboard, Player Owned, Score and Tx history
-router.get('/:id', async function (req, res) {
+router.get( '/:id', async function ( req, res ) {
     try {
         const _id = req.params.id
-        const profileData = await User.find({address: _id})
-        if(profileData.length === 0) {
-            res.send({})
+        const profileData = await User.find( { address: _id } )
+        if ( profileData.length === 0 ) {
+            res.send( {} )
             return
         }
         const allData = await User.find()
         let scores = []
-        allData.map((item, index1) => {
-            scores.push(item.score)
-        })
-        scores = scores.sort((function(a, b) {
-            return parseInt(a) - parseInt(b);
-        }))
+        allData.map( ( item, index1 ) => {
+            scores.push( item.score )
+        } )
+        scores = scores.sort( ( function ( a, b ) {
+            return parseInt( a ) - parseInt( b );
+        } ) )
         scores.reverse()
-        let score1 = profileData[0].score
-        let index1 = scores.indexOf(score1)
-        let profileData1 = {...profileData,"leaderboard":index1+1}
-        res.send(profileData1)
-    } catch (error) {
-        console.log(error.message)
-        res.status(500).json("Some error occured!")      
+        let score1 = profileData[ 0 ].score
+        let index1 = scores.indexOf( score1 )
+        let profileData1 = { ...profileData, "leaderboard": index1 + 1 }
+        res.send( profileData1 )
+    } catch ( error ) {
+        console.log( error.message )
+        res.status( 500 ).json( "Some error occured!" )
     }
-})
+} )
+
+// Patch request to add tokenIds 
+router.patch( "/:id/", async function ( req, res ) {
+    try {
+        const playerid = req.params.id
+        const playerId11 = req.body
+        const playerId1 = playerId11.playeerid
+        let player = await Player.findOneAndUpdate( { playerId: playerid }, { $push: { tokenIds: playerId1 } } );
+        res.send( player );
+    } catch ( error ) {
+        console.log( error.message );
+        res.status( 500 ).json( "Some error occured!" );
+    }
+} );
 
 module.exports = router
